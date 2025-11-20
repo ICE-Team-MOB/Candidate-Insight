403a77470a86ec654f12aed936a7981b14360b2a
Enot2239 <funydedpool@gmail.com>
2025-11-20 20:21:26 +0100

    first component

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,10 +1,15 @@
 
+import Misha from "../components/misha";
 import TestComponent from "../components/TestComponent";
 
 const TestPage = ()  => {
     return (
         <>
-            <TestComponent text='test'/>
+            <TestComponent text='test '/>
+            <br />
+            <h1>Chlen</h1>
+            <br />
+            <Misha text='zalupa pashalko 1488'/>
         </>
     );
 };
