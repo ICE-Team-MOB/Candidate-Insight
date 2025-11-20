3998588eeb2902339fa3d0a8d9527b1aba037488
XHIKALO <dmytro.barylko@nure.ua>
2025-11-20 21:20:56 +0200

    first components

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,10 +1,12 @@
 
+import Dima from "../components/dima";
 import TestComponent from "../components/TestComponent";
 
 const TestPage = ()  => {
     return (
         <>
             <TestComponent text='test'/>
+            <Dima text='i love you'/>
         </>
     );
 };
